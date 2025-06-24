3ea053f9546ac61e7677ec012967d3065bd66f48
gregpolack <grzelock@gmail.com>
2025-06-24 09:11:26 +0200

    added basic collision logic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroidfield import AsteroidField
@@ -33,6 +34,12 @@ def main():
         
         player.draw(screen)
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides(player):
+                print("Game over!")
+                sys.exit()
+                
         pygame.display.flip()
         dt = clock.tick(60) / 1000 
 
